4c3659bae6e283aa1a07cec44445c99e87963d60
RishinKrishna <rishinkrishna07@gmail.com>
2023-07-08 19:00:05 +0530

    inital setup

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,55 @@
+import useFetch from "../../hooks/useFetch";
+import "./featured.css";
+
+const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrid,london"
+  );
+
+  return (
+    <div className="featured">
+      {loading ? (
+        "Loading please wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?cs=srgb&dl=pexels-pixabay-258154.jpg&fm=jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} properties</h2>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://www.berjayahotel.com/sites/default/files/colombo_30.jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Madrid</h1>
+              <h2>{data[1]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>London</h1>
+              <h2>{data[2]} properties</h2>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
