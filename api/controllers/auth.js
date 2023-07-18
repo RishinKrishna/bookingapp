6bfb98bd424659f54e1d3f7022914dbc0793f895
RishinKrishna <rishinkrishna07@gmail.com>
2023-07-18 18:05:53 +0530

    Created admin, api, client files

---
+++ api/controllers/auth.js
@@ -0,0 +1,47 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
+
+    await newUser.save();
+    res.status(200).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return  res.status(401).json({message: "User Not Found"})
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return   res.status(400).json({message: "Incorrect Password"});
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res
+
+      .status(200)
+      .json({ details: { ...otherDetails }, isAdmin, token });
+  } catch (err) {
+    next(err);
+  }
+};
